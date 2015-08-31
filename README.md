b21c2b755ba8d179a9280a73315f75d4ae14231e
Julian Garritano <julian.garritano@radialpoint.com>
2015-08-31 10:43:10 -0400

    improved json response docs

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-## Introduction
+ethanifier
+==========
 
-Description of your project here.
+This is a simple Node.js built to support Ethanifier for Android!
+Deploy using this handy heroku button :)
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+Ethanifier docs available [here](https://github.com/juliangarritano/ethanifier).
--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var request = require('request');
 var SLACK_API_ENDPOINT = 'https://slack.com/api/';
 var GET_PRESENCE = 'users.getPresence';
 var USER_INFO = 'users.info';
+var SLACK_USER_ID = process.env.SLACK_USER_ID;
+var SLACK_AUTH_TOKEN = process.env.SLACK_AUTH_TOKEN;
 
 /*
  * Routes
@@ -17,22 +19,22 @@ var USER_INFO = 'users.info';
 app.get('/slackPresence', function(req, res) {
     request.post(SLACK_API_ENDPOINT + GET_PRESENCE, function(error, response, body) {
         if (!error && response.statusCode === 200) {
-            res.status(200).json(body);
+            res.status(200).json(JSON.parse(body));
         }
     }).form({
-        token: process.env.SLACK_AUTH_TOKEN,
-        user: process.env.SLACK_USER_ID
+        token: SLACK_AUTH_TOKEN,
+        user: SLACK_USER_ID
     });
 });
 
 app.get('/slackUserInfo', function(req, res) {
     request.post(SLACK_API_ENDPOINT + USER_INFO, function(error, response, body) {
         if (!error && response.statusCode === 200) {
-            res.status(200).json(body);
+            res.status(200).json(JSON.parse(body));
         }
     }).form({
-        token: process.env.SLACK_AUTH_TOKEN,
-        user: process.env.SLACK_USER_ID
+        token: SLACK_AUTH_TOKEN,
+        user: SLACK_USER_ID
     });
 });
 
